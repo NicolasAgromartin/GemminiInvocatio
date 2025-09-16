5c937be3d7086c2472dc933e002a86494dd3cce4
Nicolas <nicolas.agromartin97@gmail.com>
2025-09-15 21:44:15 -0300

    Update

---
+++ Assets/_Vicky/Scripts/GoToHome.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+
+public class GoToHome : MonoBehaviour
+{
+    public void HomeScene()
+    {
+        SceneManager.LoadSceneAsync("MainMenu");
+    }
+}
