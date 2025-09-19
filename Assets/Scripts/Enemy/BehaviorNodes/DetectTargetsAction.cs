246b598d20e0e13562b970e791aa1784c7932d0f
agusslesca <agustinlescanouni@gmail.com>
2025-09-18 23:52:49 -0300

    zombie

--- Assets/Scripts/Enemy/BehaviorNodes/DetectTargetsAction.cs
+++ Assets/Scripts/Enemy/BehaviorNodes/DetectTargetsAction.cs
@@ -4,9 +4,6 @@ using UnityEngine;
 using Action = Unity.Behavior.Action;
 using Unity.Properties;
 
-
-
-
 [Serializable, GeneratePropertyBag]
 [NodeDescription(name: "DetectTargets", story: "Use [TargetsDetector] to get [SelectedTarget]", category: "Action", id: "a0f9e7f830cbf043aa1624af6b84048d")]
 public partial class DetectTargetsAction : Action
@@ -14,40 +11,47 @@ public partial class DetectTargetsAction : Action
     [SerializeReference] public BlackboardVariable<TargetsDetector> TargetsDetector;
     [SerializeReference] public BlackboardVariable<GameObject> SelectedTarget;
 
-
-
-
     protected override Status OnStart()
     {
-        TargetsDetector.Value.OnTargetsUpdated += ChangeSelectedTarget;
-        return Status.Running;
+        // Asegúrate de que el objeto TargetsDetector exista antes de intentar usarlo.
+        if (TargetsDetector.Value != null)
+        {
+            TargetsDetector.Value.OnTargetsUpdated += ChangeSelectedTarget;
+            return Status.Running;
+        }
+
+        // Si el objeto es nulo, la acción no puede ejecutarse.
+        Debug.LogError("TargetsDetector is not assigned in the Behavior Tree.");
+        return Status.Failure;
     }
+
     protected override Status OnUpdate()
-    { 
+    {
         return Status.Running;
     }
+
     protected override void OnEnd()
     {
-        //Debug.Log("Unsiscribed to the target detector");
-        TargetsDetector.Value.OnTargetsUpdated -= ChangeSelectedTarget;
+        
+        // Se añade una verificación de nulidad para evitar el error.
+        if (TargetsDetector.Value != null)
+        {
+            TargetsDetector.Value.OnTargetsUpdated -= ChangeSelectedTarget;
+        }
     }
-
-
-
-
 
     private void ChangeSelectedTarget(GameObject newTarget)
     {
-        SelectedTarget.Value = newTarget;
-
-        if (SelectedTarget.Value == null)
+        if (SelectedTarget != null)
         {
-            return;
+            SelectedTarget.Value = newTarget;
+
+            if (SelectedTarget.Value == null)
+            {
+                return;
+            }
+
+            if (newTarget != SelectedTarget.Value) SelectedTarget.Value = newTarget;
         }
-
-        if(newTarget != SelectedTarget.Value) SelectedTarget.Value = newTarget;
-
-        //Debug.Log($"{TargetSelected.Value} <---___--> {SelectedTarget.Value}");
     }
 }
-
